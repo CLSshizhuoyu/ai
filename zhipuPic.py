15a4439a319613e3b13b573bf90796d7529ad886
CLSshizhuoyu <132541042+CLSshizhuoyu@users.noreply.github.com>
2026-01-16 18:12:55 +0800

    Refactor main function to include image size options
    Added size parameter to main function for image generation.

--- zhipuPic.py
+++ zhipuPic.py
@@ -1,8 +1,11 @@
+from api_key_manager import get_api_key
 import webbrowser
 import requests
 import json
 import time
-def main(head_question, mode):
+size = ["1024x1024", "768x1344", "864x1152", "1344x768", "1152x864", "1440x720", "720x1440"]
+
+def main(head_question, mode, si = 1):
     if head_question:
         en = head_question
     else:
@@ -11,12 +14,13 @@ def main(head_question, mode):
 
     url = "https://open.bigmodel.cn/api/paas/v4/images/generations"
     headers = {
-        "Authorization": "0a364811e3394194897d5cf61e933a94.NATjpJv8z77Eo2l9",
+        "Authorization": get_api_key(name="Pic"),
         "Content-Type": "application/json"
     }
     data = {
         "model": "CogView-3-Flash",
-        "prompt": en
+        "prompt": en,
+        "size" : size[si - 1]
     }
 
     response = requests.post(url, headers=headers, json=data)
